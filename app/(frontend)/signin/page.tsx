9858679c474afde2497e9cb9450e720227781254
tarekshawky <tar@xtnd.net>
2024-04-05 02:50:28 +0200

    install next-auth and  add signin form

---
+++ app/(frontend)/signin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Form from './Form'
+export default async function signIn() {
+  return (
+    <div>
+        <Form/>
+    </div>
+  )
+}
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -25,9 +25,7 @@ const Menu = () => {
                 </Link>
             </div>
             <div>
-            <button className="block rounded-md bg-teal-600 px-5 py-3.5 text-sm font-medium text-white transition hover:bg-teal-700">
-                        Sign In
-                    </button>
+            <Link href='/signin'  className="block rounded-md bg-teal-600 px-5 py-3.5 text-sm font-medium text-white transition hover:bg-teal-700"> Sign In</Link>
             </div>
     
         </div>
