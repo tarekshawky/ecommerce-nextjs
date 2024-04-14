b367e0c9dd7594865ec4258446bcfde1c2327d35
tarekshawky <tar@xtnd.net>
2024-04-14 04:45:44 +0200

    update dbconnect

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
 async function dbConnect() {
-    try{
-        await mongoose.connect(process.env.MONGODB_URI!)
-    }catch(error){
-        throw new Error('Connection failed!')
+    try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined');
+        }
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+        });
+
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Connection to MongoDB failed:', error);
+        throw new Error('Connection failed!');
     }
 }
 
