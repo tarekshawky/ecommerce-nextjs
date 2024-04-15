471ab2de617788b4a1f37db134f96d2dc55de127
tarekshawky <tar@xtnd.net>
2024-04-15 04:16:23 +0200

    Update seed

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -1,21 +1,32 @@
-import data from "@/lib/data";
+import data from "@/lib/data"; // Import data
 import dbConnect from "@/lib/dbConnect";
 import ProductModel from "@/lib/models/ProductModel";
 import UserModel from "@/lib/models/UserModel";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (request:NextRequest)=> {
-    const {users,products} = data
-    await dbConnect()
-    await UserModel.deleteMany()
-    await UserModel.insertMany(users)
+export const GET = async (request: NextRequest) => {
+  const { users, products } = data; // Destructure data
 
-    await ProductModel.deleteMany()
-    await ProductModel.insertMany(products)
+  try {
+    await dbConnect();
+
+    // Delete existing users and insert new users
+    await UserModel.deleteMany();
+    await UserModel.insertMany(users);
+
+    // Delete existing products and insert new products
+    await ProductModel.deleteMany();
+    await ProductModel.insertMany(products);
 
     return NextResponse.json({
-        message: 'Seeded Successfully',
-        users,
-        products
-    })
-}
+      message: 'Seeded Successfully',
+      users,
+      products,
+    });
+  } catch (error) {
+    console.error(error); // Log any errors
+    return NextResponse.json({
+      message: 'Error seeding data',
+    });
+  }
+};
